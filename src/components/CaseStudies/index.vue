a734c9ff74b88e30653816a54a4055114fb48bb4
miltonknight <miltonknight@163.com>
2020-11-13 17:00:36 +0800

    提取首页成功案例为全局组件

---
+++ src/components/CaseStudies/index.vue
@@ -0,0 +1,95 @@
+<template>
+  <!-- 成功案例 -->
+  <div class="side-main">
+    <div class="side-head">
+      <h3>
+        <svg-icon icon-class="hdot" class-name="card-panel-icon" />成功案例
+        <a href="/#/case-studies/index">更多案例</a>
+      </h3>
+    </div>
+    <div class="side-body">
+      <div class="side-img-box">
+        <img src="@/assets/img/side-demo4.png" alt="" />
+        <i class="badge badge-blue">NEW</i>
+      </div>
+      <div class="side-body">
+
+        <!-- <router-link v-for="item in Artical" :key="item.article_id" to="'/#/case-studies/artical?id=' + item.article_id'"> -->
+        <a v-for="item in Artical" :key="item.article_id" :href="'/#/case-studies/artical?id=' + item.article_id" >
+          <div class="slist-box">
+            <div class="slist-box-img">
+              <img :src="item.small_img_path" />
+            </div>
+            <p>{{ item.title }}</p>
+            <span>{{ item.create_time }}</span>
+          </div>
+        </a>
+        <!-- </router-link> -->
+        
+      </div>
+    </div>
+  </div>
+  <!-- 成功案例end -->
+</template>
+
+<script>
+console.log("@/components/CaseStudies/index is loaded~~~~~~~~~~~~~~~~~~~");
+import { postData } from "@/api/common";
+import { LaobingUrl } from "@/api/laobing_url";
+
+export default {
+  // name: "MaHomeheader",
+  name: "CaseStudies",
+  data() {
+    return {
+      Artical: [],
+      url: LaobingUrl.index_artical,
+      param: {
+        sort_id: 32 // 版块id=32成功案例
+      }
+    }
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methods: { 
+    postDataFromUI(url, data) {
+      return new Promise((resolve, reject) => {
+        postData(url, data)
+          .then(response => {
+            const { code, msg, data } = response;
+            if (code === 20000 && data != null) {
+              console.log("Index Case Studies Response:", data);
+              resolve(data);
+            } else {
+              reject(msg);
+              this.$message({
+                message: "没有查询到数据",
+                type: "success"
+              });
+            }
+          })
+          .catch(error => {
+            console.log(error);
+            this.$message({
+              message: error,
+              type: "success"
+            });
+          });
+      });
+    },
+    fetchData() {
+      this.postDataFromUI(this.url, this.param)
+        .then(response => {
+          this.Artical = response;
+          console.log("homepage Case Studies Json:::::::::")
+          console.log(response)
+        });
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
