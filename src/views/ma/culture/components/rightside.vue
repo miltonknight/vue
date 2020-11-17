61516510b3a6c31ed8fc66c9f43220e71886bc05
miltonknight <miltonknight@163.com>
2020-11-17 15:33:44 +0800

    新增culture相关layouts views

---
+++ src/views/ma/culture/components/rightside.vue
@@ -0,0 +1,182 @@
+<!-- 老兵驿站右侧 -->
+<template>
+  <div>
+    <!-- 作者信息 -->
+    <div class="side-main">
+      <div class="artical-side">
+        <div class="writer-box">
+          <div class="writer-head">
+            <img src="@/assets/img/head.png" alt="">
+          </div>
+          <div class="writer-info">
+            王大壮<p><span>特邀作者</span></p>
+          </div>
+          <p class="writer-intro">老兵老兵网官方特邀作者。多年写作经验，谙熟老兵文化事业。</p>
+        </div>  
+      </div>
+      <div class="writer-staff">
+        <div class="writer-staff-boxes">
+          <router-link to="">
+            <img src="@/assets/img/writer-box-1.png" alt="">
+            <span>我的文章</span>
+          </router-link>
+        </div>
+        <div class="writer-staff-boxes">
+          <router-link to="">
+            <img src="@/assets/img/writer-box-2.png" alt="">
+            <span>我的草稿</span>
+          </router-link>
+        </div>
+        <div class="writer-staff-boxes">
+          <router-link to="">
+            <img src="@/assets/img/writer-box-3.png" alt="">
+            <span>我的评论</span>
+          </router-link>
+        </div>
+        <div class="writer-staff-boxes">
+          <router-link to="">
+            <img src="@/assets/img/writer-box-4.png" alt="">
+            <span>开始写作</span>
+          </router-link>
+        </div>
+      </div>
+      
+    </div>
+    <!-- 作者信息end -->
+
+    <!-- 热门活动 -->
+    <div class="side-main">
+      <div class="side-head">
+        <h3>
+          <svg-icon icon-class="hdot" class-name="card-panel-icon" />热门活动<a href="">更多活动</a>
+        </h3>
+      </div>
+      <div class="side-body">
+        <div class="side-img-box">
+          <img src="@/assets/img/side-demo.png" alt="" />
+          <h3>阅遍山河 看世界”老兵摄影大赛</h3>
+          <p>活动时间：10月20日 - 12月20日</p>
+          <i class="badge badge-blue">活动</i>
+        </div>
+        <div class="side-img-box">
+          <img src="@/assets/img/side-demo2.png" alt="" />
+          <h3>“云”舞兵清云舞蹈大赛通知</h3>
+          <p>活动时间：10月20日 - 12月20日</p>
+          <i class="badge badge-blue">活动</i>
+        </div>
+        <div class="side-img-box">
+          <img src="@/assets/img/side-demo3.png" alt="" />
+          <h3>全军野战文艺创演活动</h3>
+          <p>活动时间：10月20日</p>
+          <i class="badge badge-orange">晚会</i>
+        </div>
+      </div>
+    </div>
+    <!-- 最新活动end -->
+  </div>    
+</template>
+
+<script>
+console.log("@/views/ma/culture/components/rightside.vue is loaded~~~~~~~~~~~~~~");
+export default {
+  name: "CultureRightSide",
+  components: {},
+  data() {
+    return {}
+  }
+  // ,
+  // computed: {
+  //   // message() {}
+  // },
+  // created: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.artical-side {
+  border: 1px solid #f5f7f9;
+  border-top: 3px solid #4696a3;
+  border-bottom: none;
+  border-radius: 3px;
+  width: 100%;
+  padding: 15px 15px;
+
+  .writer-box {
+    width: 100%;
+    clear: both;
+    overflow: hidden;
+    position: relative;
+    // border-bottom: 1px solid #f5f7f9;
+
+    .writer-head {
+      width: 78px;
+      height: 78px;
+      float: left;
+      margin-right: 20px;
+
+      img {
+        width: 78px;
+        height: 78px;
+      }
+    }
+    .writer-info {
+      font-size: 18px;
+      font-weight: bold;
+      width: 100%;
+      padding-top: 10px;
+
+      span {
+        font-size: 11px;
+        font-weight: normal;
+        color: #e08714;
+        border: 1px solid #e08714;
+        padding: 3px 7px;
+        border-radius: 3px;
+      }
+    }
+    .writer-follow {
+      width: 67px;
+      height: 26px;
+      line-height: 26px;
+      font-size: 13px;
+      border-radius: 3px;
+      background: #e08714;
+      color: #fff;
+      text-align: center;
+      position: absolute;
+      top: 10px;
+      right: 0;
+    }
+    .writer-intro {
+      font-size: 13px;
+      line-height: 1.4;
+      color: #787878;
+      margin: 30px 0 0 0;
+    }
+  }
+}
+.writer-staff {
+  width: 100%;
+  background-color: #f1f0f6;
+  padding: 15px 0;
+  display: flex;
+
+  .writer-staff-boxes {
+    flex-grow: 1; 
+    width: 35px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    img {
+      display: block;
+      width: 35px;
+      height: 35px;
+      margin: 0 auto 5px;
+    }
+    span {
+      font-size: 10px;
+    }
+  }
+}
+</style>
---
+++ src/views/ma/culture/on-tao/index.vue
@@ -0,0 +1,242 @@
+<!-- 老兵文化 -->
+<template>
+  <div>
+    <el-row :gutter="24">
+      <!-- 左侧内容 -->
+      <el-col :span="17">
+        <div class="grid-content bg-purple">
+          <!-- <router-view :key="key" /> -->
+
+          <!-- components:components/focus -->
+          <culture-focus />
+          
+          <!-- list -->
+          <div class="list-container">
+            <div class="list-main">
+              <div class="list-head list-head-sub-culture">
+                <!-- <ul class="section-title">
+                  <li class="is-active" @click="searchBySort()">老兵论道</li>
+                  <li @click="searchBySort()">休闲养生</li>
+                  <li @click="searchBySort()">医学前沿</li>
+                  <li @click="searchBySort()">户外运动</li>
+                </ul> -->
+                <template>
+                  <el-tabs v-model="activeName" @tab-click="handleClick">
+                    <el-tab-pane label="老兵论道" name="56"></el-tab-pane>
+                    <el-tab-pane label="休闲养生" name="57"></el-tab-pane>
+                    <el-tab-pane label="医学前沿" name="58"></el-tab-pane>
+                    <el-tab-pane label="户外运动" name="59"></el-tab-pane>
+                  </el-tabs>
+                </template>
+              </div>
+              <div class="list-body">
+                <!-- 列表循环 -->
+                <!-- <a v-for="item in ArticalList.list" :key="item.id" :href="'/courier-station/artical?id=' + item.soldier_station_id" class="list-item"> -->
+                <a v-for="item in ArticalList.list" :key="item.id" :href="'/#/culture/on-tao/artical?id=' + item.article_id + '&sort_id=' + item.sort_id" class="list-item">
+                  <div class="list-body-img">
+                    <img :src="item.img_path" />
+                    <i class="badge">{{ item.sort_name }}</i>
+                  </div>
+                  <div class="list-body-text">
+                    <h3>{{ item.title }}</h3>
+                    <p>{{ item.introduction }}</p>
+                    <div class="list-text-cate">
+                      <span class="tip">{{ item.sort_name }}</span>
+                      <span class="time">
+                        <i class="el-icon-time"></i>{{ item.create_time }}
+                      </span>
+                    </div>
+                  </div>
+                </a>
+
+                <pagination
+                  background
+                  layout="prev, pager, next"
+                  v-show="total > 0"
+                  :total="total"
+                  :page.sync="listQuery.page"
+                  :limit.sync="listQuery.limit"
+                  @pagination="fetchData"
+                />
+                
+              </div>
+            </div>
+            <!-- list end -->
+          </div>
+
+        </div>
+      </el-col>
+      <!-- 左侧内容end -->
+
+      <!-- 右侧内容 -->
+      <el-col :span="7">
+        <div class="grid-content bg-purple">
+          <div class="side-section">
+            
+            <!-- components:components/rightside -->
+            <culture-right-side />
+
+          </div>
+        </div>
+      </el-col>
+      <!-- 右侧内容end -->
+    </el-row>
+  </div>
+</template>
+
+<script>
+console.log("./views/ma/culture/index is loaded~~~~~~~~~~~~~~~~~~~~");
+import CultureFocus from "@/views/ma/culture/components/focus";
+import CultureRightSide from "@/views/ma/culture/components/rightside"
+import Pagination from "@/components/Pagination";
+import { postData } from "@/api/common";
+import { LaobingUrl } from "@/api/laobing_url";
+
+export default {
+  // name: 'MaHomeHeader',
+  // components: { MaHomeheader },
+  name: "Culture",
+  components: {
+    CultureFocus, // 焦点图
+    CultureRightSide,
+    Pagination
+  },
+  data() {
+    return {
+      activeName: '56', // tab active
+      //文章列表接口地址、接收数组
+      url: LaobingUrl.modular_artical_list,
+      ArticalList: [],
+      // Pagination
+      total: 0,
+      list: null,
+      listLoading: true,
+      listQuery: {
+        sort_id: 56, // 版块id
+        page: 1,
+        limit: 10
+      }
+    };
+  },
+  // computed: { },
+  created: function() {
+    this.fetchData();
+  },
+  mounted() {
+  },
+  methods: { 
+    handleClick(tab, event) {
+      console.log(tab, event);
+      this.searchBySort(tab.name);
+    },
+    postDataFromUI(url, data) {
+      return new Promise((resolve, reject) => {
+        postData(url, data)
+          .then(response => {
+            const { code, msg, data } = response;
+            if (code === 20000) {
+              console.log("Get CS List Response:", data);
+              resolve(data);
+            }
+            // this.$message({
+            //   message: msg,
+            //   type: "success"
+            // });
+          })
+          .catch(error => {
+            console.log(error);
+            this.$message({
+              message: error,
+              type: "success"
+            });
+          });
+      });
+    },
+    fetchData() {
+      this.listLoading = true;
+      this.postDataFromUI(LaobingUrl.modular_artical_list, this.listQuery)
+        .then(response => {
+          this.ArticalList = response;
+          this.total = response.total;
+          this.list = response.list;
+          this.listLoading = false;
+        })
+        .catch(error => {
+          this.listLoading = false;
+          console.log(error);
+        });
+    },
+    searchBySort(sortId) {
+      this.listQuery.sort_id = sortId;
+      this.listQuery.page = 1;
+      this.listQuery.limit = 10;
+      this.fetchData();
+    }
+  }
+};
+</script>
+
+<style lang="scss" >
+
+/******* new home page ***************/
+.el-row {
+  margin-right: 0 !important;
+
+  .el-col.el-col-17 {
+    padding-right: 0 !important;
+  }
+  .el-col.el-col-7 {
+    padding-right: 0 !important;
+  }
+}
+
+.list-head-sub-culture {
+  border-bottom: 1px solid #d7d7d7!important;
+  margin-bottom: 30px!important;
+
+  .section-title {
+    display: flex;
+    margin: 0;
+    padding: 0;
+
+    li {
+      flex-grow: 7;
+      text-align: center;
+      list-style: none;
+      height: 38px;
+      cursor: pointer;
+    }
+    li:hover, li.is-active {
+      border-bottom: 3px solid #e08714;
+    }
+  }
+}
+.list-container .list-main .list-head {
+  height: auto;
+}
+.list-head-sub-culture {
+  .el-tabs__nav {
+      display: flex;
+
+    .el-tabs__item {
+      width: 180z px!important;
+      flex-grow: 1;
+      text-align: center;
+      font-size: 16px;
+    }
+  }
+  .el-tabs__item:hover {
+    color: #e08714;
+  }  
+  .el-tabs__item.is-active {
+    color: #e08714;
+  }
+  .el-tabs__active-bar {
+    background-color: #e08714;
+    height: 3px;
+  }
+  .el-tabs__item:hover {
+    color: #e08714;
+  }
+}
+</style>
