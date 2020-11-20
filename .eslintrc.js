aa294f2a3802d528ed0dba30d344655e198277ad
miltonknight <miltonknight@163.com>
2020-11-20 17:48:12 +0800

    优化相关规则，减少报错提示，但还是存在自封标签错误提示的bug。e.g. div i span

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,20 @@ module.exports = {
         "allowFirstLine": false
       }
     }],
+    // self defined
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'generator-star-spacing': 'off',
+    "vue/html-self-closing": ["off",{
+      "html": {
+        "void": "never",
+        "normal": "any",
+        "component": "any"
+      },
+      "svg": "always",
+      "math": "always"
+    }],
+    // self defined end
     "vue/singleline-html-element-content-newline":"off",
     "vue/multiline-html-element-content-newline":"off",
     "vue/name-property-casing": ["error", "PascalCase"],
