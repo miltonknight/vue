f7a55ad941b3e4ff3d30605d4e86f2a3f8a32da9
miltonknight <miltonknight@163.com>
2020-12-15 10:03:13 +0800

    + 顶部推荐位更改为全局组件

---
+++ src/components/BbsTop/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- 顶部推荐 -->
+  <div class="side-main">
+    <div class="top-section">
+      <div class="rcm-bbs">
+        <img src="@/assets/img/bbs.png" alt="" />
+        <i class="badge badge-blue">论坛</i>
+      </div>
+      <div class="rcm-section">
+        <a href="/#/culture/on-tao/index">老兵论道</a>
+        <a href="">休闲养生</a>
+        <a href="">医学前沿</a>
+        <a href="">户外运动</a>
+      </div>
+    </div>
+  </div>
+  <!-- 顶部推荐end -->
+</template>
+
+<script>
+console.log("Components：@/BbsTop is loaded");
+
+export default {
+  // name: "MaHomeheader",
+  name: "BbsTop",
+  data() {
+    return { }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
